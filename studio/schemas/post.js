31452e599f7d5f6c9ca26a8580b87278f41438b2
taranengermaar <taran@sanity.io>
2020-09-14 12:10:18 +0200

    Added new schema types

---
+++ studio/schemas/post.js
@@ -0,0 +1,42 @@
+export default {
+    name: "post",
+    type: "document",
+    title: "Blogpost",
+    fields: [
+      {
+        name: "title",
+        type: "string",
+        title: "Title",
+      },
+      {
+        name: "excerpt",
+        type: "text",
+        title: "Excerpt",
+      },
+      {
+        name: "body",
+        type: "text",
+        title: "Body",
+      },
+     {
+      name: "mainimage",
+      type: "image",
+      title: "Main image",
+      options: {
+        hotspot: true
+      },
+      fields: [
+        {
+          name: "alternativeText",
+          type: "string",
+          title: "Alternative text",
+          options: {
+            isHighlighted: true // <-- make this field easily accessible
+          },
+
+        }
+      ]
+
+     } 
+    ],
+  }
