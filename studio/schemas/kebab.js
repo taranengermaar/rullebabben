9c8aa411c1c9d19c4ff3cca15457c8df2cb37b0c
taranengermaar <taran@sanity.io>
2020-09-23 11:17:38 +0200

    updated anmeldelse

--- studio/schemas/kebab.js
+++ studio/schemas/kebab.js
@@ -1,29 +1,31 @@
-export default{
-    name: "kebab",
-    type: "document",
-    title: "Kebab",
-    fields: [
-      {
-        name: "title",
-        type: "string",
-        title: "Title",
-      },
-      {
-        name: "excerpt",
-        type: "text",
-        title: "Excerpt",
-      },
-      {
-        name: "body",
-        type: "text",
-        title: "Body",
-      },
-     {
+export default {
+  name: "kebab",
+  type: "document",
+  title: "Kebab",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Shop",
+    },
+    {
+      name: "excerpt",
+      type: "text",
+      title: "Excerpt",
+      description: "Summary of the result",
+    },
+    {
+      name: "bacground",
+      type: "text",
+      title: "About",
+      description: "Short history and background",
+    },
+    {
       name: "mainimage",
       type: "image",
       title: "Main image",
       options: {
-        hotspot: true
+        hotspot: true,
       },
       fields: [
         {
@@ -31,12 +33,20 @@ export default{
           type: "string",
           title: "Alternative text",
           options: {
-            isHighlighted: true // <-- make this field easily accessible
+            isHighlighted: true, // <-- make this field easily accessible
           },
-
-        }
-      ]
-
-     } 
-    ],
-  }
+        },
+      ],
+    },
+    {
+      name: "menu",
+      type: "string",
+      title: "Ordered from the menu",
+    },
+    {
+      name: "improvements",
+      type: "text",
+      title: "Suggestions for improvements",
+    },
+  ],
+};
